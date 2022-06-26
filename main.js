543f20829d9613802f08dbea8b88de3bcbdb5181
rhgoda <asdfxoaka@gmail.com>
2022-06-26 14:46:26 +0300

    1

---
+++ main.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const path = require('path');
+const fetch = require('node-fetch');
+const puppeteer = require('puppeteer-core');
+const app = express();
+const port = 3000;
+
+
+//=================================SERV
+app.use((req, res, next) => {
+  res.set('Cache-Control', 'no-store')
+  next()
+})
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.get('/example.png', function(req, res) {
+  res.sendFile(path.join(__dirname, '/example.png'));
+});
+
+app.listen(port);
+console.log("Говно запущено на порте " + port)
+
+
+//==============================BOT
+const { Telegraf } = require('telegraf');
+bot = new Telegraf(tg_token)
+const AnalId = [];
+
+
+
+
+
+
+
+
+
+
+
+//======================UPDATE
+function update() {
+  (async () => {
+    const browser = await puppeteer.launch({executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'});
+    const page = await browser.newPage();
+    await page.goto('https://aliexpress.ru/item/1005002907849570.html?spm=a2g2w.productlist.i3.2.7f6125a4zfKXg2&sku_id=12000022722268689');
+    //await page.goto('https://yandex.com');
+    await page.screenshot({path: 'example.png', clip: {x:50, y:230, width:700, height:500}});
+    await browser.close();
+  })();
+
+  // вот тут постинг
+}
+ctx.telegram.sendMediaGroup(ctx.from.id, [
+  {
+    type: "photo",
+    media:
+      "example.png"
+  },
+]); // так же будет работать да
+
+update(); // 
+setInterval(update, 600000);
+
