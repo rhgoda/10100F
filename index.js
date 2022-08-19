36c98ce9632add1f366453f36f4e0982cb161d1d
adoghr <adoghr.w@gmail.com>
2022-08-19 12:29:55 +0300

    router adaptation

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express"
+import router from "./router.js"
+import http from "http"
+import https from  "https"
+
+let app = express()
+
+app.use(router)
+ 
+http.createServer(app).listen(porthttp);
+https.createServer({}, app).listen(porthttps);
