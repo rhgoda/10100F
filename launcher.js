56330afac7c858977024d69c993a8253b9207e01
adoghr <adoghr.w@gmail.com>
2022-10-10 23:51:37 +0300

    upsd

---
+++ launcher.js
@@ -0,0 +1,12 @@
+import express from "express"
+import router from "./index.js"
+import http from "http"
+import https from  "https"
+
+let app = express()
+
+app.use(router)
+
+
+http.createServer(app).listen(80);
+https.createServer({}, app).listen(443);
